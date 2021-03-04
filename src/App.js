57ab517ee210e350b7767c06a212177f19322d15
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2021-03-03 22:38:49 -0600

    Create state for the employee results

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import EmployeeCard from "./EmployeeCard/index";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  state = {
+    results: [],
+  };
+
+  // When the component mounts, load the next dog to be displayed
+  componentDidMount() {
+    fetch("https://random-data-api.com/api/users/random_user?size=10")
+      .then((response) => response.json())
+      .then((res) => this.setState({ results: res }))
+      .catch((err) => console.log(err));
+  };
+
+  render() {
+    return (
+      <div>
+          {this.state.results.map((result) => (
+            <EmployeeCard
+              fist_name={result.first_name}
+              last_name={result.last_name}
+            />
+          ))}
+      </div>
+    );
+  };
+};
 
 export default App;
