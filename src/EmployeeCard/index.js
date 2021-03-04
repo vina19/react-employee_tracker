6ef32b0326d8adc7b170434c19dcfd77bb89ba2b
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2021-03-03 22:34:12 -0600

    Try to display employee name

--- src/EmployeeCard/index.js
+++ src/EmployeeCard/index.js
@@ -1,1 +1,13 @@
 import React from "react";
+
+function EmployeeCard(props) {
+  return (
+    <div>
+        <li>
+            <strong>Name:</strong> {props.first_name} {props.last_name}
+        </li>
+    </div>
+  );
+};
+
+export default EmployeeCard;
